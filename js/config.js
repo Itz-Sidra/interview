db91726302bcc24ea9548ad65cd0b9e4a7c4fb19
JAHANGIR SIDRA TABREZ <sidrajahangir93@gmail.com>
2026-01-11 10:40:40 +0530

    shifted to gcp

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@ export const API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://evalvate-ai-backend.onrender.com";
+    : "https://evalvate-backend-862980960928.asia-south1.run.app";
