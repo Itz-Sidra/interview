0c399ddc119e04c3d149d295ac6044c1fa65fcbd
JAHANGIR SIDRA TABREZ <sidrajahangir93@gmail.com>
2026-01-11 09:12:46 +0530

    routes

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 export const API_BASE =
-  window.location.hostname === "localhost"
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
     : "https://evalvate-ai-backend.onrender.com";
