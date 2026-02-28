35c38aacea4cfb36ad8e2d63a32c0b323b52e02e
JAHANGIR SIDRA TABREZ <sidrajahangir93@gmail.com>
2026-02-28 08:44:04 +0530

    to run locally

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,8 @@ app.use(cors({
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "http://localhost:5502",
+    "http://127.0.0.1:5502",
     "http://localhost:3000",
     "https://evalvate.dev",
     "https://www.evalvate.dev",
@@ -21,7 +23,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(bodyParser.json());
+app.use(express.json());
+// app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
 app.get('/profile', authenticateToken, (req, res) => {
@@ -35,7 +38,7 @@ app.get("/health", (req, res) => {
 });
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 if (!PORT) {
   throw new Error("PORT not defined. Cloud Run requires process.env.PORT");
